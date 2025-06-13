84e02a4ea8a9cb31fb2d33b43155ca3b6f510a81
Anthony <ajackerm@mtu.edu>
2025-06-13 04:47:08 -0400

    update

--- HGCF/HGCF/settings.py
+++ HGCF/HGCF/settings.py
@@ -12,9 +12,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv # type: ignore
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv(BASE_DIR.parent / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
